4144a040e49b04f81286fdebe7e02acf166100b9
Timothy Long <st7gg4wk7b@privaterelay.appleid.com>
2026-08-29 14:08:04 -0400

    add footer, styles in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './index.css';
 
 import Header from './components/Header.tsx';
+import Footer from './components/Footer.tsx';
 
 
 function App() {
   return(
+    <>
     <Header />
+    <Footer />
+    </>
   );
 }
 
