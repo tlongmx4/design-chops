0006de93e518896b6eb8cc996c54a4fa4c6b253b
Timothy Long <st7gg4wk7b@privaterelay.appleid.com>
2026-08-29 13:02:55 -0400

    add Header.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+import Header from './components/Header.tsx';
+
+
+function App() {
+  return(
+    <Header />
+  );
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+    return(
+        <header className="Header">
+            <h1 className="title">Lucent</h1>
+            <p className="sub-title">AI &middot; Automation &middot; Infrastructure</p>
+            <nav className="navbar">
+                <ul className="nav-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Mission</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+      
+}
+
+export default Header
